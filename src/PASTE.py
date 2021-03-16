c35d9e65224f19d92a0c03dbfba979ef93aa17a1
mrland99 <max.ruikang.land@gmail.com>
2021-03-16 14:31:23 -0400

    improve center align runtime
    optimized objective function R calculation

--- src/PASTE.py
+++ src/PASTE.py
@@ -65,19 +65,21 @@ def center_align(A, layers, lmbda, alpha = 0.1, n_components = 15, threshold = 0
     W = model.fit_transform(A.gene_exp)
     H = model.components_
     center_coordinates = A.coordinates
+    
     # Initialize D_center
     center_layer = STLayer(np.dot(W, H), center_coordinates)
     center_layer.gene_exp.columns = common_genes
     D_center = generateDistanceMatrix(center_layer, center_layer)
+    
+    # Minimize R
     iteration_count = 0
     R = 0
     R_diff = 100
     while R_diff > 0.001 and iteration_count < 10:
         print("Iteration: " + str(iteration_count))
-        pi, M = center_ot(W, H, layers, center_coordinates, common_genes, alpha)
+        pi, r = center_ot(W, H, layers, center_coordinates, common_genes, alpha)
         W, H = center_NMF(W, H, layers, pi, lmbda, n_components)
-
-        R_new = objective_function(layers, pi, M, D, D_center, lmbda, alpha)
+        R_new = np.dot(r,lmbda)
         iteration_count += 1
         R_diff = abs(R - R_new)
         print("R - R_new: " + str(R_diff))
@@ -91,12 +93,12 @@ def center_ot(W, H, layers, center_coordinates, common_genes, alpha):
     center_layer.gene_exp.columns = common_genes
 
     pi = []
-    M = []
+    r = []
     for l in layers:
-        p, m = pairwise_align2(center_layer, l, alpha = alpha)
+        p, r_q = pairwise_align2(center_layer, l, alpha = alpha)
         pi.append(p)
-        M.append(m)
-    return pi, M
+        r.append(r_q)
+    return pi, np.array(r)
 
 def center_NMF(W, H, layers, pi, lmbda, n_components):
     n = W.shape[0]
@@ -106,49 +108,17 @@ def center_NMF(W, H, layers, pi, lmbda, n_components):
     H_new = model.components_
     return W_new, H_new
 
-
-def objective_function(layers, pi, M, D, D_center, lmbda, alpha):
-    """
-    Calculates objective function R we are trying to minimize.
-    
-    param: layers - List of STLayers used to calculate center alignment
-    param: pi - List of ot maps between (center layer, layer_q)
-    param: M - List of cost matrices between (center layer, layer_q)
-    param: D - List of local distance matrices for each layer_q
-    param: D_center - local distance matrix of center_layer
-    param: lmbda - List of probability weights assigned to each Layer
-    param: alpha - determines weight assigned to each term in function
-    
-    return: R - objective value
-    """
-    R = 0
-    for q in range(len(layers)):
-        first_term = np.sum(np.multiply(pi[q], M[q]))
-        # calculate second term
-        t = np.nonzero(pi[q])
-        second_term = 0
-        for c1 in range(len(t[0])):
-            for c2 in range(len(t[0])):
-                i = t[0][c1]
-                j = t[1][c1]
-                k = t[0][c2]
-                l = t[1][c2]
-                second_term += ((D_center.iloc[i, k] - D[q].iloc[j, l])**2)*pi[q][i,j]*pi[q][k,l]
-        R += lmbda[q]*((1-alpha)*first_term + alpha*second_term)
-    return R
-
-
 def pairwise_align2(layer1, layer2, alpha = 0.1):
     """
     Calculates and returns optimal alignment of two layers. Assumes count matrix is preprocessed.
-    EXACT SAME as pairwise_align, but also returns M.
+    EXACT SAME as pairwise_align, but also returns r_q, to optimize calculation of R.
     
     param: layer1 - STLayer object of first layer
     param: layer2 - STLayer object of second layer
     param: alpha - Alignment tuning parameter. Note: 0 ≤ alpha ≤ 1
     
     return: pi - alignment of spots
-    return: M - kl_divergence similarity matrix
+    return: r_q - Part of objective function value R between center layer and layer q
     """
     layer1 = layer1.copy()
     layer2 = layer2.copy()
@@ -160,7 +130,4 @@ def pairwise_align2(layer1, layer2, alpha = 0.1):
     a = np.ones((layer1.gene_exp.shape[0],))/layer1.gene_exp.shape[0]
     b = np.ones((layer2.gene_exp.shape[0],))/layer2.gene_exp.shape[0]
     pi, logw = ot.gromov.fused_gromov_wasserstein(M, D1.to_numpy(), D2.to_numpy(), a, b, loss_fun='square_loss', alpha= alpha, verbose=False, log=True)
-    return pi, M
-
-
-
+    return pi, logw['fgw_dist'] #r_q
