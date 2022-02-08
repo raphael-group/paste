fix distance matrix calculation

src/paste/PASTE.py
@@ -41,7 +41,7 @@ def pairwise_align(sliceA, sliceB, alpha = 0.1, dissimilarity='kl', use_rep = No
     # Calculate expression dissimilarity
     A_X, B_X = to_dense_array(extract_data_matrix(sliceA,use_rep)), to_dense_array(extract_data_matrix(sliceB,use_rep))
     if dissimilarity.lower()=='euclidean' or dissimilarity.lower()=='euc':
-        M = distance_matrix(A_X, B_X)
+        M = scipy.spatial.distance.cdist(A_X,B_X) #distance_matrix(A_X, B_X)
     else:
         s_A = A_X + 0.01
         s_B = B_X + 0.01